0f07ff330f42e8618d38db5659fd543181c524cf
Byron Francisco Chanax Itzep <1byron1999@gmail.com>
2024-07-15 10:33:14 -0700

    fix task icons and clear scss styles

--- client/src/app/tasks/components/task-card/task-card.component.ts
+++ client/src/app/tasks/components/task-card/task-card.component.ts
@@ -1,24 +1,38 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Task } from '../../interfaces/Task';
 import { TasksService } from '../../tasks.service';
 import { RouterModule } from '@angular/router';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon'
 
 @Component({
   selector: 'task-card',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatCheckboxModule, MatMenuModule, MatButtonModule, MatIconModule],
   templateUrl: './task-card.component.html',
   styleUrl: './task-card.component.scss'
 })
 export class TaskCardComponent {
 
-  constructor(private tasksServices : TasksService) {}
-
+  constructor(private tasksServices: TasksService) { }
+  
   @Input('task')
-  public task : Task = {} as Task
+  public task: Task = {} as Task
 
-  public onDelete(id : string) {
+
+  public onDelete(id: string) {
     this.tasksServices.deleteTaskById(id)?.subscribe()
+  };
+
+  public onClickDone(id: string) {
+    this.tasksServices.handleDone(id).subscribe()
+  };
+
+  public onClickFavorite(id: string) {
+
+    this.tasksServices.handleFavorite(id).subscribe()
   }
 
 }
